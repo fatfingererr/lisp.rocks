1853500d7a62a172f1607c042ab92d396047d55e
ffe <fatfingererr@gmail.com>
2017-12-26 23:18:30 +0800

    Update blog

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -1,8 +1,12 @@
 @echo off
-cd /d "..\docs"
+cd /d %~dp0
+cd /d "..\..\fatfingererr.github.io"
 for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
-xcopy /e /i /v /h /k "..\resources\public" ".\"
-cd /d ".."
+xcopy /e /i /v /h /k "..\lisp.rocks\resources\public" "."
+git add -A
+git commit -m "Update blog"
+git push origin master --force
+cd "..\lisp.rocks"
 git add -A
 git commit -m "Update blog"
 git push origin master --force

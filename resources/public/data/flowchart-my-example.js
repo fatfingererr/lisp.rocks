2901ed3afeabba4111a4ff211593c8d1ffe87197
ffe <fatfingererr@gmail.com>
2017-12-30 14:46:30 +0800

    Update blog

--- resources/public/data/flowchart-my-example.js
+++ resources/public/data/flowchart-my-example.js
@@ -1,6 +1,36 @@
 window.onload = function() {
-    var diagram2 = flowchart.parse('\n'+
-                                   'st=>start: Start:>http://www.google.com[blank]\n' +
-                                   '\n');
-    diagram2.drawSVG('diagram2');
+    var diagram = flowchart.parse('st=>start: Start:>http://www.google.com[blank]\n' +
+        'e=>end:>http://www.google.com\n' +
+        'op1=>operation: My Operation1\n' +
+        'sub1=>subroutine: My Subroutine1\n' +
+        'cond1=>condition: Yes \n' +
+        'or No 1?\n:>http://www.google.com\n' +
+        'op2=>operation: My Operation2\n' +
+        'sub2=>subroutine: My Subroutine2\n' +
+        'cond2=>condition: Yes \n' +
+        'or No 2?\n:>http://www.google.com\n' +
+        'io=>inputoutput: catch something...\n' +
+        '' +
+        'st->op1->cond1\n' +
+        'cond1(yes, right)->op2->cond2\n' +
+        'cond1(no)->op1\n' +
+        'cond2(yes, right)->io->e\n' + // conditions can also be redirected like cond(yes, bottom) or cond(yes, right)
+        'cond2(no)->op2');
+    diagram.drawSVG('diagram');
+
+    // you can also try to pass options:
+
+    diagram.drawSVG('diagram', {
+        'line-width': 3,
+        'line-length': 50,
+        'text-margin': 10,
+        'font-size': 14,
+        'font-color': 'black',
+        'line-color': 'black',
+        'element-color': 'black',
+        'fill': 'white',
+        'yes-text': 'yes',
+        'no-text': 'no',
+        'arrow-end': 'block'
+    });
 };

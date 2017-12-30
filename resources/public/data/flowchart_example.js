f4e3e0aeebe39bbf5e91c38fee1f43e01dbc0e9f
ffe <fatfingererr@gmail.com>
2017-12-30 14:14:20 +0800

    Update blog

--- resources/public/data/flowchart_example.js
+++ resources/public/data/flowchart_example.js
@@ -1,57 +1,36 @@
-  var diagram = flowchart.parse('st=>start: Start:>http://www.google.com[blank]\n' +
-      'e=>end:>http://www.google.com\n' +
-      'op1=>operation: My Operation\n' +
-      'op2=>operation: Stuff|current\n' +
-      'sub1=>subroutine: My Subroutine\n' +
-      'cond=>condition: Yes \n' + // use cond(align-next=no) to disable vertical align of symbols below
-      'or No?\n:>http://www.google.com' +
-      'c2=>condition: Good idea|rejected\n' +
-      'io=>inputoutput|request: catch something...\n' +
-      '' +
-      'st->op1(right)->cond\n' +
-      'cond(yes, right)->c2\n' + // conditions can also be redirected like cond(yes, bottom) or cond(yes, right)
-      'cond(no)->sub1(left)->op1\n' + // the other symbols too...
-      'c2(true)->io->e\n' +
-      'c2(false)->op2->e' //allow for true and false in conditionals
-  );
-  diagram.drawSVG('diagram');
+window.onload = function() {
+    var diagram = flowchart.parse('st=>start: Start:>http://www.google.com[blank]\n' +
+        'e=>end:>http://www.google.com\n' +
+        'op1=>operation: My Operation1\n' +
+        'sub1=>subroutine: My Subroutine1\n' +
+        'cond1=>condition: Yes \n' +
+        'or No 1?\n:>http://www.google.com\n' +
+        'op2=>operation: My Operation2\n' +
+        'sub2=>subroutine: My Subroutine2\n' +
+        'cond2=>condition: Yes \n' +
+        'or No 2?\n:>http://www.google.com\n' +
+        'io=>inputoutput: catch something...\n' +
+        '' +
+        'st->op1->cond1\n' +
+        'cond1(yes, right)->op2->cond2\n' +
+        'cond1(no)->op1\n' +
+        'cond2(yes, right)->io->e\n' + // conditions can also be redirected like cond(yes, bottom) or cond(yes, right)
+        'cond2(no)->op2');
+    diagram.drawSVG('diagram');
 
-  // you can also try to pass options:
+    // you can also try to pass options:
 
-  diagram.drawSVG('diagram', {
-      'x': 0,
-      'y': 0,
-      'line-width': 3,
-      'line-length': 50,
-      'text-margin': 10,
-      'font-size': 14,
-      'font-color': 'black',
-      'line-color': 'black',
-      'element-color': 'black',
-      'fill': 'white',
-      'yes-text': 'yes',
-      'no-text': 'no',
-      'arrow-end': 'block',
-      'scale': 1,
-      // style symbol types
-      'symbols': {
-          'start': {
-              'font-color': 'red',
-              'element-color': 'green',
-              'fill': 'yellow'
-          },
-          'end': {
-              'class': 'end-element'
-          }
-      },
-      // even flowstate support ;-)
-      'flowstate': {
-          // 'past' : { 'fill' : '#CCCCCC', 'font-size' : 12},
-          // 'current' : {'fill' : 'yellow', 'font-color' : 'red', 'font-weight' : 'bold'},
-          // 'future' : { 'fill' : '#FFFF99'},
-          'request': { 'fill': 'blue' } //,
-          // 'invalid': {'fill' : '#444444'},
-          // 'approved' : { 'fill' : '#58C4A3', 'font-size' : 12, 'yes-text' : 'APPROVED', 'no-text' : 'n/a' },
-          // 'rejected' : { 'fill' : '#C45879', 'font-size' : 12, 'yes-text' : 'n/a', 'no-text' : 'REJECTED' }
-      }
-  });
+    diagram.drawSVG('diagram', {
+        'line-width': 3,
+        'line-length': 50,
+        'text-margin': 10,
+        'font-size': 14,
+        'font-color': 'black',
+        'line-color': 'black',
+        'element-color': 'black',
+        'fill': 'white',
+        'yes-text': 'yes',
+        'no-text': 'no',
+        'arrow-end': 'block'
+    });
+};

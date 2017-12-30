c3bbc3540e4798723e96aaa93212cbcd322babae
ffe <fatfingererr@gmail.com>
2017-12-30 14:40:17 +0800

    Update blog

--- resources/templates/data/flowchart_my_example.js
+++ resources/templates/data/flowchart_my_example.js
@@ -2,5 +2,5 @@ window.onload = function() {
     var diagram2 = flowchart.parse('\n'+
                                    'st=>start: Start:>http://www.google.com[blank]\n' +
                                    '\n');
-    diagram.drawSVG('diagram2');
+    diagram2.drawSVG('diagram2');
 };

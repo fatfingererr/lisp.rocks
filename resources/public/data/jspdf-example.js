c6379b6d109507e9fa8ee029d5a5f84babe83134
ffe <fatfingererr@gmail.com>
2017-12-30 16:04:06 +0800

    Update blog

--- resources/public/data/jspdf-example.js
+++ resources/public/data/jspdf-example.js
@@ -1,15 +1,11 @@
 document.getElementById("toPdfBtn").onclick = function () {
-  alert("即将打印，样式会有变化，但不妨碍您继续编辑");
-
     var doc = new jsPDF();
 
-    // 添加文本‘Download PDF’
-    doc.text('[  ] 1. tttt', 10, 10);
-    doc.text('[  ] 2. aaaa', 10, 20);
-    doc.text('[  ] 3. tttt', 10, 30);
-    doc.text('[  ] 4. bbbb', 10, 40);
-    doc.text('[  ] 5. tttt', 10, 50);
+    doc.text('[  ] 1. item1', 10, 10);
+    doc.text('[  ] 2. item2', 10, 20);
+    doc.text('[  ] 3. item3', 10, 30);
+    doc.text('[  ] 4. item4', 10, 40);
+    doc.text('[  ] 5. item5', 10, 50);
 
-    doc.save('a4.pdf');
-
+    doc.save('jspdf-example.pdf');
 };
